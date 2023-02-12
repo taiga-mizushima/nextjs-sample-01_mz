83b32b11d8c70c643e49768f3bd7b05f2617fc67
taiga-mizushima <mizusima@nci.co.jp>
2023-02-12 20:04:41 +0900

    1st commit

---
+++ fast_api/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from routers import route_todo, route_auth
+from schemas import SuccessMsg
+
+app = FastAPI()
+app.include_router(route_todo.router)
+app.include_router(route_auth.router)
+
+@app.get("/", response_model=SuccessMsg)
+def root():
+    return {"message": "Welcome to Fast API"}
---
+++ fast_api/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Todo(BaseModel):
+    id: str
+    title: str
+    description: str
+
+class TodoBody(BaseModel):
+    title: str
+    description: str
+
+class SuccessMsg(BaseModel):
+    message: str
+
+class UserBody(BaseModel):
+    email: str
+    password: str
+
+class UserInfo(BaseModel):
+    id: Optional[str] = None
+    email: str
